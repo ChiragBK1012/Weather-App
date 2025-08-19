b0a945ad26018f6b26bd2ad62e7ae7250e18f763
Chirag <chiragbk1012@gmail.com>
2025-08-20 00:15:42 +0530

    Weather App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Home from "./pages/Home";
 
 export default function App() {
-  return (
-    <div className="min-h-screen bg-zinc-50 text-zinc-900 dark:bg-zinc-900 dark:text-zinc-100">
-      <Home />
-    </div>
-  );
+    return (
+        <div className="min-h-screen bg-zinc-50 text-zinc-900 dark:bg-zinc-900 dark:text-zinc-100">
+            <Home />
+        </div>
+    );
 }
